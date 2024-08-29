0b90234ba819a998e8a071c774666c6a7ca4f16a
Omkar (Ahaan) Waghmare <91741647+omkar7075@users.noreply.github.com>
2024-08-29 19:33:16 +0530

    Feedbacks.jsx

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -33,7 +33,7 @@ const Feedbacks = () => {
 
             <button
               className="duration-300 hover:bg-sky-900 border hover:text-gray-50 bg-gray-50 font-semibold text-sky-800 px-3 py-2 flex flex-row items-center gap-3"
-              onClick={`{https://drive.google.com/file/d/1bESVPiyB2CvQhKyL-wDixynRfSphgyCF/view}`}
+              onClick={downloadCV()}
             >
               Download CV
               <svg
